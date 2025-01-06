55ba0185746fd7c14470f8279bba1078fe54e3b5
codeCraftCorpo <codeCraftCorpo@gmail.com>
2025-01-06 00:20:59 -0800

    drift

--- routes/messages.py
+++ routes/messages.py
@@ -2,15 +2,19 @@ from fastapi import APIRouter
 from app.messageHander import Message, Messages
 from dependencies import client
 from app.messageHander import get_message_handler, post_message_handler
+from pydantic import BaseModel
+
+class MessageRequest(BaseModel):
+    core: str
 
 router = APIRouter()
 
 @router.get("/messages")
 def get_message():
-  return get_message_handler()
+    return get_message_handler()
 
 @router.post("/messages")
-def post_message(body: dict):
-  return post_message_handler(body["core"])
+def post_message(message: MessageRequest):
+    return post_message_handler(message.core)
 
 
