3b785dd60f0272de1ac5ab7701b90f4ef3627195
codeCraftCorpo <codeCraftCorpo@gmail.com>
2025-01-05 19:36:51 -0800

    drift

--- routes/messages.py
+++ routes/messages.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from ..app.messageHander import Message, Messages
+from app.messageHander import Message, Messages
 
 
 router = APIRouter()
 
 @router.get("/messages")
 def get_messages():
-  return Message(core="no glory in wrappers", isUser=False)
+  return Message(core="lets all love claude", isUser=False)
